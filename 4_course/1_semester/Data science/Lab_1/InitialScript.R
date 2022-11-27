85acc70d57043fb6b72bfc24673dd108f799efeb
DenDunno <60221683+DenDunno@users.noreply.github.com>
2022-11-27 21:57:10 +0200

    data science lab

--- 4_course/1_semester/Data science/Lab_1/InitialScript.R
+++ 4_course/1_semester/Data science/Lab_1/InitialScript.R
@@ -2,6 +2,8 @@
 
 library(psych) 
 library(moments)
+library(magrittr)
+library(dplyr)
 
 setwd("D:/Business/University/University_3_course/4_course/1_semester/Data science/Lab_1")
 data = read.table("A2.txt", header = FALSE, sep = ",", dec = ".")
@@ -14,16 +16,26 @@ getmode = function(x)
   uniqv[which.max(tabulate(match(x, uniqv)))]
 }
 
-GiniMd = function(x, na.rm=FALSE) {
-  if(na.rm) {
+GiniMd = function(x, na.rm=FALSE) 
+{
+  if(na.rm) 
+  {
     k = is.na(x)
     if(any(k)) x <- x[! k]
   }
+  
   n =length(x)
   if(n < 2) return(NA)
   w = 4 * ((1 : n) - (n - 1) / 2) / n / (n - 1)
   sum(w * sort(x - mean(x)))  ## center for numerical stability only
 }
+
+DispersAnalasis <- function(values) 
+{
+  disan <- ((1 / (length(values) - 1)) * sum(((values - sum(values))*(values - sum(values)))))
+  return(disan)
+}
+
 
 preliminaryProcessing = function(x, channelIndex)
 {
@@ -43,4 +55,54 @@ preliminaryProcessing = function(x, channelIndex)
   
   print("Тест:")
   shapiro.test(x)
+}
+
+normalization = function(x)
+{
+  tempData = as.data.frame(scale(data)) 
+  print(paste("Cереднє = ", mean(tempData$V1)))
+  print(paste("Дисперсія = ", var(tempData$V1)))
+}
+
+anova = function(x)
+{
+  temp = data %>% summarise_each(funs(DispersAnalasis))
+  max = max(temp)
+  sum = sum(temp)
+  g = max / sum
+  
+  print(paste("Max = ", max))
+  print(paste("Sum = ", sum))
+  print(paste("G = ", g))
+  print(paste("ga(k, n) = ", 0.153))
+}
+
+twoWayAnova = function(x)
+{
+  data1 = data[1:1000, ] 
+  data2 = data[1001:2000, ] 
+  data3 = data[2001:3000, ] 
+  data4 = data[3001:4000, ] 
+  data5 = data[4001:5000, ] 
+  
+  mean1 = data1 %>% summarise_each(funs(mean))
+  mean2 = data2 %>% summarise_each(funs(mean))
+  mean3 = data3 %>% summarise_each(funs(mean))
+  mean4 = data4 %>% summarise_each(funs(mean))
+  mean5 = data5 %>% summarise_each(funs(mean))
+  
+  #print(mean1)
+  #print(mean2)
+  #print(mean3)
+  #print(mean4)
+  #print(mean5)
+  
+  sumByColumnTable = data.frame(mean1, mean2, mean3, mean4, mean5)
+  sumByColumnTable.
+}
+
+fur = function(x)
+{
+  
+
 }
--- 4_course/1_semester/Data science/Lab_1/Lab_1.R
+++ 4_course/1_semester/Data science/Lab_1/Lab_1.R
@@ -28,20 +28,193 @@ preliminaryProcessing(data$V10, 10)
 preliminaryProcessing(data$V11, 11)
 preliminaryProcessing(data$V12, 12)
 
-as.data.frame(scale(data)) #normalization
+
+# Normalization
+normalization(data)
 
 
 # ANOVA
-tempData = data
-summary(aov(tempData))
-aov(values   ~ ind, stack(tempData))
+anova(data)
 
+
+# Two-way ANOVA
+data1 = data[1:1000, ] 
+data2 = data[1001:2000, ] 
+data3 = data[2001:3000, ] 
+data4 = data[3001:4000, ] 
+data5 = data[4001:5000, ] 
+
+mean1 = data1 %>% summarise_each(funs(mean))
+mean2 = data2 %>% summarise_each(funs(mean))
+mean3 = data3 %>% summarise_each(funs(mean))
+mean4 = data4 %>% summarise_each(funs(mean))
+mean5 = data5 %>% summarise_each(funs(mean))
+
+print(mean1)
+print(mean2)
+print(mean3)
+print(mean4)
+print(mean5)
+
+sumByColumnTable = rbind(mean1, mean2, mean3, mean4, mean5)
+columnsSum = colSums(aboba)
+
+sumMean1 = sum(mean1)
+sumMean2 = sum(mean2)
+sumMean3 = sum(mean3)
+sumMean4 = sum(mean4)
+sumMean5 = sum(mean5)
+
+Q1 = sum(mean1 * mean1) + 
+     sum(mean2 * mean2) + 
+     sum(mean3 * mean3) + 
+     sum(mean4 * mean4) + 
+     sum(mean5 * mean5)
+     
+Q22 <- function(values){
+ x1 <- (sum(values * values))
+ return (x1)
+}
+     
+Q21 <- Q22(sumByColumnTable)
+Q2 <- (1/5 * Q21)
+print(Q2)
+
+
+Q3 <- ((1 / 12) * (sum(mean1) * sum(mean1) + 
+                   sum(mean2) * sum(mean2) +
+                   sum(mean3) * sum(mean3) +
+                   sum(mean4) * sum(mean4) +
+                   sum(mean5) * sum(mean5)))
+print(Q3)
+
+Q4 <- ((1 / 60) * sum(sumByColumnTable) * sum(sumByColumnTable))
+print(Q4)
+
+S0_2 <- ((Q1 + Q4 - Q2 - Q3) / 44)
+print(S0_2)
+
+SA_2 <- ((Q2 - Q4) / 11)
+print(SA_2)
+
+SB_2 <- ((Q3 - Q4) / 4)
+print(SB_2)
+
+print(SA_2 / S0_2)
+print(SB_2 / S0_2)
+df(0.05, 11, 44)
+df(0.05, 4, 44)
+
+
+# Фур'є
+furStat1 <- fft(data$V1)
+furStat2 <- fft(data$V2)
+furStat3 <- fft(data$V3)
+furStat4 <- fft(data$V4)
+furStat5 <- fft(data$V5)
+furStat6 <- fft(data$V6)
+furStat7 <- fft(data$V7)
+furStat8 <- fft(data$V8)
+furStat9 <- fft(data$V9)
+furStat10 <- fft(data$V10)
+furStat11 <- fft(data$V11)
+furStat12 <- fft(data$V12)
+
+invFurStat1 <- fft(furStat1)
+invFurStat2 <- fft(furStat2)
+invFurStat3 <- fft(furStat3)
+invFurStat4 <- fft(furStat4)
+invFurStat5 <- fft(furStat5)
+invFurStat6 <- fft(furStat6)
+invFurStat7 <- fft(furStat7)
+invFurStat8 <- fft(furStat8)
+invFurStat9 <- fft(furStat9)
+invFurStat10 <- fft(furStat10)
+invFurStat11 <- fft(furStat11)
+invFurStat12 <- fft(furStat12)
+
+spectrum(furStat1, type = "l", col="red")
+spectrum(invFurStat1, type = "l", col="red")
+
+spectrum(furStat2, type = "l", col="red")
+spectrum(invFurStat2, type = "l", col="red")
+
+spectrum(furStat3, type = "l", col="red")
+spectrum(invFurStat3, type = "l", col="red")
+
+spectrum(furStat4, type = "l", col="red")
+spectrum(invFurStat4, type = "l", col="red")
+
+spectrum(furStat5, type = "l", col="red")
+spectrum(invFurStat5, type = "l", col="red")
+
+spectrum(furStat6, type = "l", col="red")
+spectrum(invFurStat6, type = "l", col="red")
+
+spectrum(furStat7, type = "l", col="red")
+spectrum(invFurStat7, type = "l", col="red")
+
+spectrum(furStat8, type = "l", col="red")
+spectrum(invFurStat8, type = "l", col="red")
+
+spectrum(furStat9, type = "l", col="red")
+spectrum(invFurStat9, type = "l", col="red")
+
+spectrum(furStat10, type = "l", col="red")
+spectrum(invFurStat10, type = "l", col="red")
+
+spectrum(furStat11, type = "l", col="red")
+spectrum(invFurStat11, type = "l", col="red")
+
+spectrum(furStat12, type = "l", col="red")
+spectrum(invFurStat12, type = "l", col="red")
 
 # Correlation matrix
-cor(data, method = c("pearson", "kendall", "spearman"))
+cor(data)
+a = data$V1;
+b = data$V4;
+c = data$V10;
+d = data$V11;
+
+rab <- cor(a, b, method = "pearson")
+rac <- cor(a, c, method = "pearson")
+rad <- cor(a, d, method = "pearson")
+rbc <- cor(b, c, method = "pearson")
+rbd <- cor(b, d, method = "pearson")
+rcd <- cor(c, d, method = "pearson")
+print(rab)
+print(rac)
+print(rad)
+print(rbc)
+print(rbd)
+print(rcd)
+
+rab_c <- ((rab - rac * rbc) / sqrt((1 - rac) * (1 - rbc)))
+print(rab_c)
+
+rac_b <- ((rac - rab * rbc) / sqrt((1 - rab) * (1 - rbc)))
+print(rac_b)
+
+rab_cd <- ((rab - rac * rbc * rad * rbd) / sqrt((1 - rac) * (1 - rbc) * (1 - rad) * (1 - rbd)))
+print(rab_cd)
+
+rac_bd <- ((rac - rab * rbc * rad * rcd) / sqrt((1 - rab) * (1 - rbc) * (1 - rad) * (1 - rcd)))
+print(rac_bd)
+
+rad_bc <- ((rad - rab * rbd * rac * rcd) / sqrt((1 - rab) * (1 - rbd) * (1 - rac) * (1 - rcd)))
+print(rad_bc)
+
+ra_bc <- sqrt((rab * rab + rac * rac - 2 * rab * rac * rbc) / (1 - rbc * rbc))
+print(ra_bc)
+
+ra_bcd <- (1 - (1 - rab * rab) * (1 - rac_b * rac_b) * (1 - rad_bc * rad_bc))
+print(ra_bcd)
 
 
 #Factor analysis
-values = eigen(cor(data, method = c("pearson", "kendall", "spearman")))
-values$values
-values$vectors
+corData <- cor(data)
+eigenData <- eigen(corData)
+print(eigenData$values)
+plot(eigenData$values, type = "b", col="green")
+
+print(eigenData$vectors)
